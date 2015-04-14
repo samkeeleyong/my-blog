702594e9acb6cae32c07275819f89bbb6bcd789e
Sam Keeley Ong <sam.ong@orangeandbronze>
2015-04-14 11:56:03 +0800

    Initial commit with helloworld endpoint

---
+++ src/main/java/com/samkeeleyong/blog/domain/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.samkeeleyong.blog.domain;
+
+public class HelloWorld {
+	private String content;
+
+	public HelloWorld(String content){
+		this.content = content;
+	}
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
